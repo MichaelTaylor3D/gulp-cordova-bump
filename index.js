e046d7dbec6ea902fd46a706fbf54cc8b3838f62
MichaelTaylor <mtaylor@michaeltaylor3d.com>
2015-01-08 12:11:23 -0500

    Remove q dependancy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var gulp = require('gulp'),
     bump = require('gulp-bump'),
     gutil = require('gulp-util'),
-    q = require('q'),
+    Q = require('q')
     semver = require('semver'),
     filter = require('gulp-filter'),
     xeditor = require("gulp-xml-editor"),
@@ -13,26 +13,25 @@ var getPackageJson = function () {
 };
 
 function bumpver(version) {
+  
   gutil.log("Remember to run this before you run cordova build");
-  var deferred = Q.defer();
+  
   var pkg = getPackageJson();
   var oldVer = pkg.version;
   var newVer = semver.inc(oldVer, version);
   var jsonFilter = filter('**/*.json');
   var xmlFilter = filter('**/*.xml');
 
-  gulp.src(['./package.json', './bower.json', './config.xml'])
+  return gulp.src(['./package.json', './bower.json', './config.xml'])
     .pipe(jsonFilter)
     .pipe(bump({version: newVer}))
     .pipe(gulp.dest('./'))
     .pipe(jsonFilter.restore())
     .pipe(xmlFilter)
     .pipe(xeditor([
-        {path: '.', attr: { 'version': newVer } } 
+        { path: '.', attr: { 'version': newVer } } 
     ]))
   .pipe(gulp.dest("./dest"));
-
-  return deferred.promise;
 }
 
 module.exports = bumpver;
